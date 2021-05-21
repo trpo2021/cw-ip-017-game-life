cbe25244befd2ca46444d8238c36d65d16636f68
Ivan Zdanovich <Zdanowitch.Iv@yandex.ru>
2021-05-22 03:37:53 +0700

    Add clang-format

--- src/main/java/life/Life.java
+++ src/main/java/life/Life.java
@@ -3,7 +3,6 @@ package life;
 class Life {
   private int width;
   private int height;
-
   private boolean[][] currentGeneration;
   private boolean[][] previousGeneration;
   private int generationCount;
