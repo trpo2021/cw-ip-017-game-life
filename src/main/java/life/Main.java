321adeeadece181f812deeece9f31b3d4fbe81df
Ivan Zdanovich <Zdanowitch.Iv@yandex.ru>
2021-05-25 20:15:54 +0700

    Realise function initWorld() and write tests

--- src/main/java/life/Main.java
+++ src/main/java/life/Main.java
@@ -1,7 +1,7 @@
 package life;
 
 public class Main {
-  public static int sum(int a, int b) { return a + b; }
 
-  public static int difference(int a, int b) { return a - b; }
+  public static void main(String[] args) {
+  }
 }
